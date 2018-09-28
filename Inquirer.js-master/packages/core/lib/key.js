285776e9f2db143afb940f9b75f21d165879f780
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-09-28 16:05:47 -0700

    updated repository

--- Inquirer.js-master/packages/core/lib/key.js
+++
@@ -1,5 +0,0 @@
-exports.isUpKey = key =>
-  key.name === 'up' || key.name === 'k' || (key.name === 'p' && key.ctrl);
-
-exports.isDownKey = key =>
-  key.name === 'down' || key.name === 'j' || (key.name === 'n' && key.ctrl);
