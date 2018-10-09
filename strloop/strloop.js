5395ebf078669bd90326f814661354482eaf8f67
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-09 10:19:23 -0700

    not working, still need to work on it

--- strloop/strloop.js
+++ strloop/strloop.js
@@ -2,14 +2,6 @@ var a = [1,2,3];
 function f(a) {
 	var toEval = [];
 	var hundredList = [];
-	for (var i = 0; i < a.length; i++) {
-		for (var j = 0; j < returnList.length; j++) {
-			temp.push(returnList[returnList.length-j]+a[i]);
-			temp.push(returnList[returnList.length-j]-a[i]);
-			temp.push(returnList[returnList.length-j]*10+a[i]);
-			returnList.push(temp);
-		}
-	}
 	return toEval;
 }
 console.log(f(a));
