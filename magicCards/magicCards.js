55e71cf1efd909fbb595e6e86c5ce57a242dea16
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-09 10:14:29 -0700

    fully working

---
+++ magicCards/magicCards.js
@@ -0,0 +1,62 @@
+function setup() {
+	document.getElementById('questions').style.display = 'block';
+}
+var array = new Array(0);
+var it = 0;
+var correct = 0;
+function reset() {
+	var p = document.getElementById('questions').children[0].children[0];
+	document.getElementById('questions').style.display = 'none';
+	p.textContent = "Is your number in card 1?";
+	p.id = '1';
+	array.length = 0;
+	document.getElementById('aftertext').textContent = '';
+	document.getElementById('reset').textContent = '';
+	it = 0;
+	document.getElementById('yes').style.display = 'none';
+	document.getElementById('no').style.display = 'none';
+	document.getElementById('was i correct').style.display = 'none';
+}
+
+function f() {
+	it++;
+	var p = document.getElementById('questions').children[0].children[0];
+	var ifchecked = p.parentElement.children[1].checked;
+	if (ifchecked) {
+		array.push(Number(p.id));
+	}
+	if (it < 7) {
+		$("#questionChanger").fadeOut(700, function(){
+			var num = Number(p.id)+1;
+			num = num.toString();
+			p.textContent = "Is your number in card "+num+"?";
+			p.id = num.toString();
+			p.parentElement.children[1].checked = false;
+			p.parentElement.children[3].checked = false;
+		});
+		$("#questionChanger").fadeIn(700);
+	} else {
+		findNum(array);
+	}
+}
+function add() {
+	correct++;
+	callit();
+}
+function callit() {
+	document.getElementById('correct').textContent = 'Number of times I guessed your number correctly: ' + correct;
+	reset();
+}
+function findNum(array) {
+	var sum = 0;
+	for (var i = 0; i < array.length; i++) {
+		sum += 2**(array[i]-1);
+	}
+	document.getElementById('aftertext').textContent = "Your number is: " + sum;
+	document.getElementById('reset').textContent = "Press reset to play again.";
+	document.getElementById('yes').style.display = 'block';
+	document.getElementById('no').style.display = 'block';
+	document.getElementById('was i correct').style.display = 'block';
+	array.length = 0;
+	return sum;
+}
--- strloop/strloop.js
+++ strloop/strloop.js
@@ -1,38 +1,15 @@
-var str = 1234567890;
-function f(str) {
-	var a = str.toString().split('');
-	var b = ['', '+', '-'];
-	var insert = Math.floor(Math.random() * 3);
+var a = [1,2,3];
+function f(a) {
+	var toEval = [];
+	var hundredList = [];
 	for (var i = 0; i < a.length; i++) {
-		if (b[insert] === '' && i !== a.length-1) {
-			a[i] += a[i+1];
-			a.splice(i+1, 1);
-			i--;
-		}
-		else {
-			a.splice(i+1, 0, b[insert]);
-			i += 1;
-		}
-		insert = Math.floor(Math.random() * 3);
-	}
-	a.pop();
-	var sum = Number(a[0]);
-	for (i = 1; i < a.length; i++)  {
-		if (a[i] === '+') {
-			sum += Number(a[i+1]);
-		}else if (a[i] === '-') {
-			sum -= Number(a[i+1]);
+		for (var j = 0; j < returnList.length; j++) {
+			temp.push(returnList[returnList.length-j]+a[i]);
+			temp.push(returnList[returnList.length-j]-a[i]);
+			temp.push(returnList[returnList.length-j]*10+a[i]);
+			returnList.push(temp);
 		}
 	}
-	var list = [];
-	if (sum !== 100) {
-		f(str);
-	}
-	else {
-		if (list.includes(a.join('')) === false) {
-			list.push(a.join(''));
-		}
-		f(str);
-	}
+	return toEval;
 }
-f(str);
+console.log(f(a));
