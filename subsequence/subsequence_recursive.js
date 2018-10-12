8487104826e6ab73fc65a51d53d9561215490bab
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-12 10:22:35 -0700

    iterative and recursive

--- subsequence/subsequence_recursive.js
+++ subsequence/subsequence_recursive.js
@@ -1,1 +1,18 @@
-o
+var str1 = "halloween";
+var str2 = "all hallow's evening";
+function f(str1, str2) {
+	// BASE CASE
+	if (str1.length === 0) {
+		return true;
+	}else if (str2.length === 0) {
+		return false;
+	}
+	// RECURSIVE CASE
+	if (str1.charAt(0) === str2.charAt(0)) {
+		str1 = str1.substr(1);
+	}
+	str2 = str2.substr(1);
+	return f(str1, str2);
+}
+console.log("Is '"+str1+"' a subsequence of '"+str2+"'?");
+console.log(f(str1, str2));
