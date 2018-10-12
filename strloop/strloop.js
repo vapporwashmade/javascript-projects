9344456cb1bed96438583393fb20851988c1d771
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-11 20:49:32 -0700

    works, 33 combos

--- strloop/strloop.js
+++ strloop/strloop.js
@@ -1,7 +1,22 @@
-var a = [1,2,3];
+var a = ['1','2','3','4','5','6','7','8','9','0'];
 function f(a) {
-	var toEval = [];
-	var hundredList = [];
-	return toEval;
+	if (a.length === 2) {
+		return [a[0]+"+"+a[1], a[0]+"-"+a[1], a[0]+a[1]];
+	}
+	var ldigit = a.pop();
+	var prev = f(a);
+	var next = [];
+	for (var i = 0; i < prev.length; i++) {
+		next.push(prev[i]+"+"+ldigit, prev[i]+"-"+ldigit, prev[i]+ldigit);
+	}
+	return next;
 }
-console.log(f(a));
+var toEval = f(a);
+var hundredList = [];
+for (var i = 0; i < toEval.length; i++) {
+	if (eval(toEval[i]) === 100) {
+		hundredList.push(toEval[i]);
+	}
+}
+console.log("There are "+hundredList.length+" possible combinations that add up to 100. \nThey are: ");
+console.log(hundredList);
