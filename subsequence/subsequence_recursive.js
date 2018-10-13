9afa29aeec14755953b5bb5b0fe87da3eee57a67
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-12 18:34:34 -0700

    faster

--- subsequence/subsequence_recursive.js
+++ subsequence/subsequence_recursive.js
@@ -1,5 +1,5 @@
-var str1 = "halloween";
-var str2 = "all hallow's evening";
+var str1 = "hac";
+var str2 = "cathartic";
 function f(str1, str2) {
 	// BASE CASE
 	if (str1.length === 0) {
@@ -8,10 +8,12 @@ function f(str1, str2) {
 		return false;
 	}
 	// RECURSIVE CASE
-	if (str1.charAt(0) === str2.charAt(0)) {
-		str1 = str1.substr(1);
+	var index = str2.indexOf(str1.charAt(0));
+	if (index === -1) {
+		return false;
 	}
-	str2 = str2.substr(1);
+	str1 = str1.substr(1);
+	str2 = str2.substr(index);
 	return f(str1, str2);
 }
 console.log("Is '"+str1+"' a subsequence of '"+str2+"'?");
