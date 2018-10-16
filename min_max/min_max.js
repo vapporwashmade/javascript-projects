52be1afded87f9de0a52a8494a263d3776e311d0
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-16 16:17:48 -0700

    updated

--- min_max/min_max.js
+++ min_max/min_max.js
@@ -1,20 +1,25 @@
 var a = [1, 34, 53, 7, 100];
 // this function takes in a number array
 // it returns the maximum and the minimum of the array
-function max_min(a) {
+function max_min(a, check) {
 	var n = a.length;
 	if (n === 0) {
 		return undefined;
 	}
-	var maximum = a[0];
-	var minimum = a[0];
+	var m = a[0];
 	for (var i = 1; i < n; i++) {
-		if (a[i] > maximum) {
-			maximum = a[i];
-		} else if (a[i] < minimum) {
-			minimum = a[i];
+		var e = a[i];
+		if (check(e, m)) {
+			m = e;
 		}
 	}
-	return [maximum, minimum];
+	return m;
 }
-console.log(max_min(a));
+var max = max_min(a, function (f1, f2) {
+	return f1 > f2;
+});
+var min = max_min(a, function (f1, f2) {
+	return f1 < f2;
+});
+console.log(max);
+console.log(min);
