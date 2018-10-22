5bd998cfbef358736f9166cb747492ced0f15b91
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-21 20:29:17 -0700

    updated

--- createBinary/createBinary.js
+++ createBinary/createBinary.js
@@ -7,12 +7,11 @@ function f(n) {
 	}
 	// recursive case
 	var binArray = [];
-	var prebinArray = f(n-1);
-	for (var i = 0; i < prebinArray.length; i++) {
-		var zero = prebinArray[i] + '0';
-		var one = prebinArray[i] + '1';
-		binArray.push(zero, one);
+	var prebinArray = f(n - 1);
+	for (var i = 0; i < prebinArray.length; i ++) {
+		binArray.push(prebinArray[i] + '0', prebinArray[i] + '1');
 	}
 	return binArray;
 }
-console.log(f(4));
+
+console.log(f(8));
