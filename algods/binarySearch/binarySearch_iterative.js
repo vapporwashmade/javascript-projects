6cc1efed311bb0376dad6d0421085aa4086fbc91
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-25 15:13:11 -0700

    new directory, still working on it

--- algods/binarySearch/binarySearch_iterative.js
+++ algods/binarySearch/binarySearch_iterative.js
@@ -2,16 +2,17 @@
 // takes in a target number and a sorted array of numbers
 // it returns the first index of the element that equals target
 // the array must be sorted otherwise results are unpredictable
-function binarySearch_iterative(target, a) {
+// low is inclusive and high is exclusive
+function binarySearchIterative(target, a) {
 	var low = 0;
 	var high = a.length;
 	var mid = Math.floor(low + (high-low)/2);
 	while (low < mid && a[mid] !== target) {
 		if (a[mid] < target) {
-			low = mid;
+			low = mid + 1;
 			mid = Math.floor(low + (high-low)/2);
 		} else if (a[mid] > target) {
-			high = mid;
+			high = mid - 1;
 			mid = Math.floor(low + (high-low)/2);
 		}
 	}
@@ -24,4 +25,4 @@ var a = [];
 for (var i = 0; i < 100; i+=2) {
 	a.push(i+1);
 }
-console.log(binarySearch_iterative(1, a));
+console.log(binarySearchIterative(100, a));
---
+++ algods/space-time tradeoff/getBrightnessCalculate.js
@@ -0,0 +1,2 @@
+// takes in three values for red, green, and blue respectively
+// multiplies the values and returns the result
