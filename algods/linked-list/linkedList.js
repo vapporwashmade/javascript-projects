a1d0dd6214a4538b36fd5212e8cc9558395a218c
Apoorv Mhaswade <vappor.washmade@gmail.com>
2019-02-19 13:23:05 +0530

    unfinished reverse and sort methods implementation

--- algods/linked-list/linkedList.js
+++ algods/linked-list/linkedList.js
@@ -1,4 +1,5 @@
-// operations: inserting element in middle, deleting element, searching
+// operations: inserting element, deleting element, searching
+// TODO: reverse method, sort method
 class LinkedList {
     constructor(array) {
         var a = array.reverse();
@@ -15,6 +16,14 @@ class LinkedList {
                 }
             };
         }
+    }
+
+    predecessor(pos) {
+        var pred = this.list;
+        for (var i = 1; i < pos; i++) {
+            pred = pred.next();
+        }
+        return pred;
     }
 
     length() {
@@ -36,10 +45,7 @@ class LinkedList {
     }
 
     insert(pos, elem) {
-        var pred = this.list;
-        for (var i = 1; i < pos; i++) {
-            pred = pred.next();
-        }
+        var pred = this.predecessor(pos);
         pred.ref = {
             val : elem,
             ref : pred.next(),
@@ -50,11 +56,11 @@ class LinkedList {
                 return this.val;
             }
         };
-        return this.list;
     }
 
-    delete(pos, elem) {
-
+    delete(pos) {
+        var pred = this.predecessor(pos);
+        pred.ref = pred.next().next();
     }
 
     prepend(elem) {
@@ -68,9 +74,7 @@ class LinkedList {
                 return this.val;
             }
         };
-        return this.list;
     }
-
     print() {
         var toPrint = [];
         var node = this.list;
@@ -80,6 +84,31 @@ class LinkedList {
         }
         return toPrint;
     }
+
+    search(val) {
+        var node = this.list;
+        while (node !== undefined) {
+            if (node.val === val) {
+                return node;
+            }
+            node = node.ref;
+        }
+        return undefined;
+    }
+
+    reverse(node) {
+         // BASE CASE
+        if (node.ref === undefined) {
+            return node;
+        }
+            // RECURSIVE CASE4
+        var prevNode = this.reverse(node.ref);
+        prevNode.ref = node;
+        prevNode.ref.ref = undefined;
+        return prevNode.ref;
+    }
 }
 var linkedList = new LinkedList([1, 2, 3, 4, 5]);
-console.log(linkedList.next().next().value());
+console.log(linkedList.print());
+console.log(linkedList.reverse(linkedList.list));
+console.log(linkedList.print());
