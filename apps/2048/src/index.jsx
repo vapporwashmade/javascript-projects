885a2e6b014d3f8866d80490d665773b5cc3d96a
apoorv <apoorv@kedar.dyndns-home.com>
2019-03-21 07:55:12 +0530

    update 2048

--- apps/2048/src/index.jsx
+++ apps/2048/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
-import styles from './styles';
 
 class Number extends React.Component {
     constructor(props) {
@@ -13,7 +12,7 @@ class Number extends React.Component {
     }
 
     render() {
-        return (<div style={this.state.number === undefined ? styles.emptyStyle : styles.numberStyle}>
+        return (<div className={this.state.number === undefined ? 'empty' : 'number'}>
             <p>{this.state.number}</p>
         </div>);
     }
@@ -26,53 +25,92 @@ class Table extends React.Component {
             squares: []
         };
         this.create = this.create.bind(this);
+        this.down = this.down.bind(this);
     }
 
     create() {
-        var squares = [];
+        console.log(this.state.squares);
+        var a = [];
         for (var i = 0; i < 16; i++) {
-            squares.push(null);
+            a.push(undefined);
         }
+        console.log('i do a fill');
         i = Math.floor(Math.random() * 8);
-        squares[i] = 2;
+        a[i] = 2;
         i = Math.floor(Math.random() * 8) + 8;
-        squares[i] = 2;
+        a[i] = 2;
+        console.log(a);
         this.setState({
-            squares: squares
-        });
+            squares: a
+        }, this.render);
     }
 
     componentWillMount() {
         this.create();
     }
 
+    down() {
+        console.log('x');
+        // function move() {
+        //     var moves = [];
+        //     for (var i = 4; i < 16; i += 4) {
+        //         if (this.state.squares[i - 4] === null && this.state.squares[i] !== null) {
+        //             this.state.squares[i - 4] = this.state.squares[i];
+        //             this.state.squares[i] = null;
+        //             document.getElementById((i - 4).toString()).innerText = this.state.squares[i - 4].toString();
+        //             document.getElementById((i).toString()).innerText = this.state.squares[i].toString();
+        //         }
+        //         if (this.state.squares[i - 3] === null && this.state.squares[i + 1] !== null) {
+        //             this.state.squares[i - 3] = this.state.squares[i + 1];
+        //             this.state.squares[i + 1] = null;
+        //             document.getElementById((i - 3).toString()).innerText = this.state.squares[i - 3].toString();
+        //             document.getElementById((i + 1).toString()).innerText = this.state.squares[i + 1].toString();
+        //         }
+        //         if (this.state.squares[i - 2] === null && this.state.squares[i + 2] !== null) {
+        //             this.state.squares[i - 2] = this.state.squares[i + 2];
+        //             this.state.squares[i + 2] = null;
+        //             document.getElementById((i - 2).toString()).innerText = this.state.squares[i - 2].toString();
+        //             document.getElementById((i + 2).toString()).innerText = this.state.squares[i + 2].toString();
+        //         }
+        //         if (this.state.squares[i - 1] === null && this.state.squares[i + 3] !== null) {
+        //             this.state.squares[i - 1] = this.state.squares[i + 3];
+        //             this.state.squares[i + 3] = null;
+        //             document.getElementById((i - 1).toString()).innerText = this.state.squares[i - 1].toString();
+        //             document.getElementById((i + 3).toString()).innerText = this.state.squares[i + 3].toString();
+        //         }
+        //     }
+        // }
+        //
+        // move();
+    }
+
     render() {
         return (<div>
-                <table>
+                <table className="table">
                     <tbody>
                         <tr>
-                            <td><Number number={this.state.squares[0]}/></td>
-                            <td><Number number={this.state.squares[1]}/></td>
-                            <td><Number number={this.state.squares[2]}/></td>
-                            <td><Number number={this.state.squares[3]}/></td>
+                            <td className="td"><Number number={this.state.squares[0]}/></td>
+                            <td className="td"><Number number={this.state.squares[1]}/></td>
+                            <td className="td"><Number number={this.state.squares[2]}/></td>
+                            <td className="td"><Number number={this.state.squares[3]}/></td>
                         </tr>
                         <tr>
-                            <td><Number number={this.state.squares[4]}/></td>
-                            <td><Number number={this.state.squares[5]}/></td>
-                            <td><Number number={this.state.squares[6]}/></td>
-                            <td><Number number={this.state.squares[7]}/></td>
+                            <td className="td"><Number number={this.state.squares[4]}/></td>
+                            <td className="td"><Number number={this.state.squares[5]}/></td>
+                            <td className="td"><Number number={this.state.squares[6]}/></td>
+                            <td className="td"><Number number={this.state.squares[7]}/></td>
                         </tr>
                         <tr>
-                            <td><Number number={this.state.squares[8]}/></td>
-                            <td><Number number={this.state.squares[9]}/></td>
-                            <td><Number number={this.state.squares[10]}/></td>
-                            <td><Number number={this.state.squares[11]}/></td>
+                            <td className="td"><Number number={this.state.squares[8]}/></td>
+                            <td className="td"><Number number={this.state.squares[9]}/></td>
+                            <td className="td"><Number number={this.state.squares[10]}/></td>
+                            <td className="td"><Number number={this.state.squares[11]}/></td>
                         </tr>
                         <tr>
-                            <td><Number number={this.state.squares[12]}/></td>
-                            <td><Number number={this.state.squares[13]}/></td>
-                            <td><Number number={this.state.squares[14]}/></td>
-                            <td><Number number={this.state.squares[15]}/></td>
+                            <td className="td"><Number number={this.state.squares[12]}/></td>
+                            <td className="td"><Number number={this.state.squares[13]}/></td>
+                            <td className="td"><Number number={this.state.squares[14]}/></td>
+                            <td className="td"><Number number={this.state.squares[15]}/></td>
                         </tr>
                     </tbody>
                 </table>
@@ -80,7 +118,20 @@ class Table extends React.Component {
     }
 }
 
-ReactDOM.render(<Table/>, document.getElementById('root'));
+class Game extends React.Component {
+    newGame() {
+        this.refs.table.create();
+    }
+
+    render() {
+        return (<div>
+            <Table ref="table"/>
+            <button id="newGame" onClick={this.newGame.bind(this)}>New Game</button>
+        </div>);
+    }
+}
+
+ReactDOM.render(<Game/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- apps/2048/src/styles.js
+++ apps/2048/src/styles.js
@@ -1,11 +1,14 @@
-var numberStyle = {
+export const numberStyle = {
     'backgroundColor': "238, 228, 218, 0.35",
     'width': '100',
     'height': '100',
     "alignContent": 'center'
 };
-var emptyStyle = {
-    'backgroundColor': "brown",
-    'width': '100',
-    'height': '100'
+export const emptyStyle = {
+    'display': 'none'
+};
+
+export const tableStyle = {
+    'borderWidth': '2px',
+    'borderColor': 'brown'
 };
