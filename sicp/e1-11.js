326c8085cc5adc286772454beea6e8d0ca50b44e
Source Academy User <No public email provided>
2023-03-24 08:00:21 +0530

    Changes made from Source Academy

--- sicp/e1-11.js
+++ sicp/e1-11.js
@@ -1,3 +1,4 @@
+// Using Source 1
 // Exercise 1.11 SICP
 // f(n) = n if n < 3
 // f(n) = f(n-1) + 2f(n-2) + 3f(n-3) if n >= 3
@@ -9,7 +10,12 @@ function f(n) {
     : f(n - 1) + (2 * f(n - 2)) + (3 * f(n - 3));
 }
 
-// Iterative implemenation of function f(n)
+// Iterative implementation of function f(n)
 function f_iter(n) {
     return 
+}
+
+// Direct calculation of f(n)
+function f_direct(n) {
+    
 }
